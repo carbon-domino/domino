eaec2ffa06032495c016d91824d35152dc64e852
André Torres de Queiroz <andretorresdequeiroz@gmail.com>
2020-08-28 22:31:40 -0300

    Update main.c
    Resolvi o conflito e adicionei o menu principal

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
+#include <stdio.h>
+#include "components/main_menu.h"
+#include "components/action_menu.h"
 #include "game.c"
 
 int main() {
-    newGame();
+
+    char option;
+
+    do {
+        showMainMenu();
+        option = getchar();
+
+        switch (option){
+        case '1': printf("Iniciando novo jogo...\n");
+            newGame();
+            break;
+        case '2': printf("Continuando\n");
+            //call the function
+            break;
+        case '3': printf("Salvando jogo...\n");
+            //call the function
+            break;
+        case '4': printf("Carregando jogo...\n");
+            //call the function
+            break;
+        case '5': printf("Regras do jogo:\n");
+            //call the function
+            break;
+        case '6': printf("Voce saiu do jogo.\n");
+            break;
+        }
+    } while(option != '6');
+    return 0;
 }
