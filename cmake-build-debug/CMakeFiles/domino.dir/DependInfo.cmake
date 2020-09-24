54dbd8c6a2b7bbbd4953ea4044a5adc3d10b14fe
Caio Baracat <caio.baracat@picpay.com>
2020-09-23 22:09:29 -0300

    adiciona build no projeto

---
+++ cmake-build-debug/CMakeFiles/domino.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/caiobaracat/Development/puc/domino/controller/gameController.c" "/Users/caiobaracat/Development/puc/domino/cmake-build-debug/CMakeFiles/domino.dir/controller/gameController.c.o"
+  "/Users/caiobaracat/Development/puc/domino/main.c" "/Users/caiobaracat/Development/puc/domino/cmake-build-debug/CMakeFiles/domino.dir/main.c.o"
+  "/Users/caiobaracat/Development/puc/domino/model/gameModel.c" "/Users/caiobaracat/Development/puc/domino/cmake-build-debug/CMakeFiles/domino.dir/model/gameModel.c.o"
+  "/Users/caiobaracat/Development/puc/domino/view/gameView.c" "/Users/caiobaracat/Development/puc/domino/cmake-build-debug/CMakeFiles/domino.dir/view/gameView.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../model"
+  "../components"
+  "../controller"
+  "../view"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/domino.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/domino.dir/controller/gameController.c.o"
+  "CMakeFiles/domino.dir/main.c.o"
+  "CMakeFiles/domino.dir/model/gameModel.c.o"
+  "CMakeFiles/domino.dir/view/gameView.c.o"
+  "domino"
+  "domino.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/domino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
